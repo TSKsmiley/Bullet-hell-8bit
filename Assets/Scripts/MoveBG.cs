2e204291e16a26a11f248a3bb84b58b850c06321
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2021-02-12 19:21:42 +0100

    healthStuff

---
+++ Assets/Scripts/MoveBG.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MoveBG : MonoBehaviour
+    {
+        public Transform tf;
+        void FixedUpdate()
+        {
+            tf.Translate(Vector3.up * -.05f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        [SerializeField]private int health = 100;
+        public Scrollbar hb;
+
+        /// <summary>
+        /// Changes the health by the given ammount
+        /// </summary>
+        public void ChangeHealth(int deltaHealth)
+        {
+            health += deltaHealth;
+            if (health <= 0) die();
+            hb.size = (float)health / 100;
+        }
+
+        /// <summary>
+        /// Overrides the current health level
+        /// </summary>
+        public void SetHealth(int newHealth)
+        {
+            health = newHealth;
+            if (health <= 0) die();
+            hb.size = (float)health / 100;
+        }
+
+        /// <summary>
+        /// Gets the current health level
+        /// </summary> 
+        public int GETHealth()
+        {
+            return health;
+        }
+
+        private void die()
+        {
+            // Restart the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
